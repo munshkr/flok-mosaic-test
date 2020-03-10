ccc8c59a799fe916907a038745f2e75d38f91381
Damián Silvani <munshkr@gmail.com>
2020-03-10 18:17:02 -0300

    Add example without blueprint

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import 'react-mosaic-component/react-mosaic-component.css'
+
+// import '@blueprintjs/core/lib/css/blueprint.css';
+// import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+
+import '../app.css'
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
