afc5276a5a8c12f30bf6e376154d6acf39626718
Damián Silvani <munshkr@gmail.com>
2020-03-10 19:49:42 -0300

    Remove blueprint

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,31 @@
-import React from 'react'
-// import { Classes } from '@blueprintjs/core';
+import React from "react";
 // import classNames from 'classnames';
 // import Head from 'next/head'
-import { Mosaic, MosaicNode, MosaicWindow, MosaicZeroState } from 'react-mosaic-component';
+import { Mosaic, MosaicWindow, MosaicZeroState } from "react-mosaic-component";
 
-// import { CloseAdditionalControlsButton } from './CloseAdditionalControlsButton';
+// import { CloseAdditionalControlsButton } from "../components/CloseAdditionalControlsButton";
 
 let windowCount = 3;
 
-/*
-const THEMES = {
-  ['Blueprint']: 'mosaic-blueprint-theme',
-  ['Blueprint Dark']: classNames('mosaic-blueprint-theme', Classes.DARK),
-  ['None']: '',
-};
-*/
+// const additionalControls = React.Children.toArray([
+//   <CloseAdditionalControlsButton />
+// ]);
 
-// const additionalControls = React.Children.toArray([<CloseAdditionalControlsButton />]);
-
-const EMPTY_ARRAY = [];
-
+// const EMPTY_ARRAY = [];
 
 class Home extends React.Component {
   state = {
     currentNode: {
-      direction: 'row',
+      direction: "row",
       first: 1,
       second: {
-        direction: 'column',
+        direction: "column",
         first: 2,
-        second: 3,
+        second: 3
       },
-      splitPercentage: 40,
+      splitPercentage: 40
     },
-    currentTheme: 'None',
+    currentTheme: "None"
   };
 
   render() {
@@ -47,9 +39,15 @@ class Home extends React.Component {
                 title={`Window ${count}`}
                 createNode={this.createNode}
                 path={path}
-                onDragStart={() => console.log('MosaicWindow.onDragStart')}
-                onDragEnd={(type) => console.log('MosaicWindow.onDragEnd', type)}
-                renderToolbar={count === 2 ? () => <div className="toolbar-example">Custom Toolbar</div> : null}
+                onDragStart={() => console.log("MosaicWindow.onDragStart")}
+                onDragEnd={type => console.log("MosaicWindow.onDragEnd", type)}
+                renderToolbar={
+                  count === 2
+                    ? () => (
+                        <div className="toolbar-example">Custom Toolbar</div>
+                      )
+                    : null
+                }
               >
                 <div className="example-window">
                   <h1>{`Window ${count}`}</h1>
@@ -60,22 +58,22 @@ class Home extends React.Component {
             value={this.state.currentNode}
             onChange={this.onChange}
             onRelease={this.onRelease}
-            className=''
+            className=""
           />
         </div>
       </React.StrictMode>
     );
   }
 
-  onChange = (currentNode) => {
+  onChange = currentNode => {
     this.setState({ currentNode });
   };
 
-  onRelease = (currentNode) => {
-    console.log('Mosaic.onRelease():', currentNode);
+  onRelease = currentNode => {
+    console.log("Mosaic.onRelease():", currentNode);
   };
 
   createNode = () => ++windowCount;
 }
 
-export default Home
+export default Home;
