01f1ab446e76669260a97c430db8e4f5ee8fd356
Damián Silvani <munshkr@gmail.com>
2020-03-11 08:37:15 -0300

    Use a custom theme based on Blueprint
    Copy fonts from BP and redeclare @fontface on theme.less, because
    fontface url can't be resolved correctly.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 // next.config.js
 const withLess = require('@zeit/next-less')
 const withCSS = require('@zeit/next-css')
+const path = require('path')
 
 module.exports = withLess(withCSS({
   cssLoaderOptions: {
--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,8 @@ import "react-mosaic-component/react-mosaic-component.css";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 
-import "../example.less";
+import "../main.less";
+import "../theme.less";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
