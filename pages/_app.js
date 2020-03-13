98f93145fd8f2e2240072aed85f6838fd28b1496
Damián Silvani <munshkr@gmail.com>
2020-03-12 21:34:06 -0300

    Import react-mosaic-component css as webpack seems to be having issues loading it

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import "react-mosaic-component/react-mosaic-component.css";
+import "../mosaic-base.css";
 
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
